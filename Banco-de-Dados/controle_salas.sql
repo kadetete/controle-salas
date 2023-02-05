b02d568898be49c6872069ac91fab3b0270a8c55
Rafael-ASS <114671081+Rafael-ASS@users.noreply.github.com>
2023-02-05 01:23:18 -0300

    Add files via upload

---
+++ Banco-de-Dados/controle_salas.sql
@@ -0,0 +1,32 @@
+#drop database controleSalas;
+
+create database controleSalas;
+use controleSalas;
+create table login(
+idlogin int auto_increment primary key,
+usuario varchar(100) not null,
+senha varchar(100) not null
+);
+
+create table cliente(
+idcliente int auto_increment primary key,
+matricula varchar(20) not null,
+nome varchar(30) not null,
+sobrenome varchar(70) not null
+);
+
+create table ambiente(
+idambiente int auto_increment primary key,
+descricao varchar(255) not null,
+tamanho varchar(10) not null
+);
+
+create table reserva(
+idreserva int auto_increment primary key,
+idcliente int,
+idambiente int,
+data_reserva date not null,
+horario_reserva datetime not null,
+constraint fk_cliente_reserva foreign key (idcliente) references cliente(idcliente),
+constraint fk_ambiente_reserva foreign key (idambiente) references ambiente(idambiente)
+);
