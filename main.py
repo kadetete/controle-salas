37b9df596fd3890078e80422d8055240a5e8fba8
Rafael-ASS <rarafaelalgusto@gmail.com>
2023-02-05 21:39:33 -0300

    att

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ from PySide6.QtWidgets import QApplication, QMainWindow, QWidget
 # You need to run the following command to generate the ui_form.py file
 #     pyside6-uic form.ui -o ui_form.py, or
 #     pyside2-uic form.ui -o ui_form.py
-from ui_form import Ui_MainWindow
-from login.login import LoginWidget
-from banco_de_dados.banco_de_dados import Login
+from banco_de_dados.banco_de_dados import *
+from ui_mainwindow import Ui_MainWindow
+from ui_login import LoginWidget
+
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self) ->None:
