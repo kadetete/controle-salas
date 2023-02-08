87497d4feab979a68d9bd84dbb72f1743f34c3a0
Guilherme Cadete <kadetesan@gmail.com>
2023-02-08 20:34:25 -0300

    add botao relatorio menu

--- ui_menu.py
+++ ui_menu.py
@@ -34,63 +34,6 @@ class MenuMainWindow(object):
         self.centralwidget.setObjectName(u"centralwidget")
         self.gridLayout = QGridLayout(self.centralwidget)
         self.gridLayout.setObjectName(u"gridLayout")
-        self.gridFrame = QFrame(self.centralwidget)
-        self.gridFrame.setObjectName(u"gridFrame")
-        self.gridFrame.setFrameShape(QFrame.Box)
-        self.gridFrame.setFrameShadow(QFrame.Raised)
-        self.gridFrame.setLineWidth(1)
-        self.gridLayout_2 = QGridLayout(self.gridFrame)
-        self.gridLayout_2.setObjectName(u"gridLayout_2")
-        self.label_3 = QLabel(self.gridFrame)
-        self.label_3.setObjectName(u"label_3")
-        font = QFont()
-        font.setFamilies([u"Versa"])
-        font.setPointSize(10)
-        self.label_3.setFont(font)
-        self.label_3.setAlignment(Qt.AlignCenter)
-
-        self.gridLayout_2.addWidget(self.label_3, 1, 3, 1, 1)
-
-        self.label_2 = QLabel(self.gridFrame)
-        self.label_2.setObjectName(u"label_2")
-        font1 = QFont()
-        font1.setFamilies([u"Versa"])
-        font1.setPointSize(10)
-        font1.setBold(False)
-        self.label_2.setFont(font1)
-        self.label_2.setAlignment(Qt.AlignCenter)
-
-        self.gridLayout_2.addWidget(self.label_2, 1, 0, 1, 1)
-
-        self.label_hora = QLabel(self.gridFrame)
-        self.label_hora.setObjectName(u"label_hora")
-
-        self.gridLayout_2.addWidget(self.label_hora, 1, 4, 1, 1)
-
-        self.label_dias = QLabel(self.gridFrame)
-        self.label_dias.setObjectName(u"label_dias")
-
-        self.gridLayout_2.addWidget(self.label_dias, 1, 1, 1, 1)
-
-        self.label = QLabel(self.gridFrame)
-        self.label.setObjectName(u"label")
-        font2 = QFont()
-        font2.setFamilies([u"Versa"])
-        font2.setPointSize(24)
-        font2.setBold(False)
-        font2.setItalic(False)
-        self.label.setFont(font2)
-        self.label.setAlignment(Qt.AlignCenter)
-
-        self.gridLayout_2.addWidget(self.label, 0, 0, 1, 5)
-
-
-        self.gridLayout.addWidget(self.gridFrame, 2, 0, 1, 2)
-
-        self.verticalSpacer = QSpacerItem(12, 5, QSizePolicy.Minimum, QSizePolicy.Fixed)
-
-        self.gridLayout.addItem(self.verticalSpacer, 3, 0, 1, 2)
-
         self.pushButton_voltar_menu = QPushButton(self.centralwidget)
         self.pushButton_voltar_menu.setObjectName(u"pushButton_voltar_menu")
         sizePolicy1 = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
@@ -98,6 +41,9 @@ class MenuMainWindow(object):
         sizePolicy1.setVerticalStretch(0)
         sizePolicy1.setHeightForWidth(self.pushButton_voltar_menu.sizePolicy().hasHeightForWidth())
         self.pushButton_voltar_menu.setSizePolicy(sizePolicy1)
+        font = QFont()
+        font.setFamilies([u"Versa"])
+        font.setPointSize(10)
         self.pushButton_voltar_menu.setFont(font)
         self.pushButton_voltar_menu.setLayoutDirection(Qt.RightToLeft)
         icon = QIcon()
@@ -111,6 +57,66 @@ class MenuMainWindow(object):
         self.pushButton_voltar_menu.setFlat(True)
 
         self.gridLayout.addWidget(self.pushButton_voltar_menu, 0, 0, 1, 1)
+
+        self.gridFrame = QFrame(self.centralwidget)
+        self.gridFrame.setObjectName(u"gridFrame")
+        self.gridFrame.setFrameShape(QFrame.Box)
+        self.gridFrame.setFrameShadow(QFrame.Raised)
+        self.gridFrame.setLineWidth(1)
+        self.gridLayout_2 = QGridLayout(self.gridFrame)
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.label_hora = QLabel(self.gridFrame)
+        self.label_hora.setObjectName(u"label_hora")
+
+        self.gridLayout_2.addWidget(self.label_hora, 2, 4, 1, 1)
+
+        self.label_2 = QLabel(self.gridFrame)
+        self.label_2.setObjectName(u"label_2")
+        font1 = QFont()
+        font1.setFamilies([u"Versa"])
+        font1.setPointSize(10)
+        font1.setBold(False)
+        self.label_2.setFont(font1)
+        self.label_2.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout_2.addWidget(self.label_2, 2, 0, 1, 1)
+
+        self.label_3 = QLabel(self.gridFrame)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setFont(font)
+        self.label_3.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout_2.addWidget(self.label_3, 2, 3, 1, 1)
+
+        self.label_dias = QLabel(self.gridFrame)
+        self.label_dias.setObjectName(u"label_dias")
+
+        self.gridLayout_2.addWidget(self.label_dias, 2, 1, 1, 1)
+
+        self.label = QLabel(self.gridFrame)
+        self.label.setObjectName(u"label")
+        font2 = QFont()
+        font2.setFamilies([u"Versa"])
+        font2.setPointSize(24)
+        font2.setBold(False)
+        font2.setItalic(False)
+        self.label.setFont(font2)
+        self.label.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout_2.addWidget(self.label, 0, 0, 1, 5)
+
+        self.pushButton_relatorio = QPushButton(self.gridFrame)
+        self.pushButton_relatorio.setObjectName(u"pushButton_relatorio")
+        sizePolicy2 = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
+        sizePolicy2.setHorizontalStretch(0)
+        sizePolicy2.setVerticalStretch(0)
+        sizePolicy2.setHeightForWidth(self.pushButton_relatorio.sizePolicy().hasHeightForWidth())
+        self.pushButton_relatorio.setSizePolicy(sizePolicy2)
+
+        self.gridLayout_2.addWidget(self.pushButton_relatorio, 1, 2, 1, 1)
+
+
+        self.gridLayout.addWidget(self.gridFrame, 2, 0, 1, 2)
 
         self.gridFrame1 = QFrame(self.centralwidget)
         self.gridFrame1.setObjectName(u"gridFrame1")
@@ -130,11 +136,11 @@ class MenuMainWindow(object):
 
         self.pushButton_Labs = QPushButton(self.gridFrame1)
         self.pushButton_Labs.setObjectName(u"pushButton_Labs")
-        sizePolicy2 = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
-        sizePolicy2.setHorizontalStretch(0)
-        sizePolicy2.setVerticalStretch(0)
-        sizePolicy2.setHeightForWidth(self.pushButton_Labs.sizePolicy().hasHeightForWidth())
-        self.pushButton_Labs.setSizePolicy(sizePolicy2)
+        sizePolicy3 = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+        sizePolicy3.setHorizontalStretch(0)
+        sizePolicy3.setVerticalStretch(0)
+        sizePolicy3.setHeightForWidth(self.pushButton_Labs.sizePolicy().hasHeightForWidth())
+        self.pushButton_Labs.setSizePolicy(sizePolicy3)
         font4 = QFont()
         font4.setFamilies([u"Versa"])
         font4.setPointSize(12)
@@ -145,33 +151,37 @@ class MenuMainWindow(object):
 
         self.pushButton_Auditorios = QPushButton(self.gridFrame1)
         self.pushButton_Auditorios.setObjectName(u"pushButton_Auditorios")
-        sizePolicy2.setHeightForWidth(self.pushButton_Auditorios.sizePolicy().hasHeightForWidth())
-        self.pushButton_Auditorios.setSizePolicy(sizePolicy2)
+        sizePolicy3.setHeightForWidth(self.pushButton_Auditorios.sizePolicy().hasHeightForWidth())
+        self.pushButton_Auditorios.setSizePolicy(sizePolicy3)
         self.pushButton_Auditorios.setFont(font4)
 
         self.gridLayout_4.addWidget(self.pushButton_Auditorios, 1, 1, 1, 1)
 
         self.pushButton_SalasEstudo = QPushButton(self.gridFrame1)
         self.pushButton_SalasEstudo.setObjectName(u"pushButton_SalasEstudo")
-        sizePolicy2.setHeightForWidth(self.pushButton_SalasEstudo.sizePolicy().hasHeightForWidth())
-        self.pushButton_SalasEstudo.setSizePolicy(sizePolicy2)
+        sizePolicy3.setHeightForWidth(self.pushButton_SalasEstudo.sizePolicy().hasHeightForWidth())
+        self.pushButton_SalasEstudo.setSizePolicy(sizePolicy3)
         self.pushButton_SalasEstudo.setFont(font4)
 
         self.gridLayout_4.addWidget(self.pushButton_SalasEstudo, 3, 1, 1, 1)
 
         self.pushButton_SalaReuniao = QPushButton(self.gridFrame1)
         self.pushButton_SalaReuniao.setObjectName(u"pushButton_SalaReuniao")
-        sizePolicy3 = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Preferred)
-        sizePolicy3.setHorizontalStretch(0)
-        sizePolicy3.setVerticalStretch(0)
-        sizePolicy3.setHeightForWidth(self.pushButton_SalaReuniao.sizePolicy().hasHeightForWidth())
-        self.pushButton_SalaReuniao.setSizePolicy(sizePolicy3)
+        sizePolicy4 = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Preferred)
+        sizePolicy4.setHorizontalStretch(0)
+        sizePolicy4.setVerticalStretch(0)
+        sizePolicy4.setHeightForWidth(self.pushButton_SalaReuniao.sizePolicy().hasHeightForWidth())
+        self.pushButton_SalaReuniao.setSizePolicy(sizePolicy4)
         self.pushButton_SalaReuniao.setFont(font4)
 
         self.gridLayout_4.addWidget(self.pushButton_SalaReuniao, 1, 0, 1, 1)
 
 
         self.gridLayout.addWidget(self.gridFrame1, 4, 0, 4, 2)
+
+        self.verticalSpacer = QSpacerItem(12, 5, QSizePolicy.Minimum, QSizePolicy.Fixed)
+
+        self.gridLayout.addItem(self.verticalSpacer, 3, 0, 1, 2)
 
         MainWindow.setCentralWidget(self.centralwidget)
 
@@ -182,12 +192,13 @@ class MenuMainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.label_3.setText(QCoreApplication.translate("MainWindow", u"Hora:", None))
-        self.label_2.setText(QCoreApplication.translate("MainWindow", u"Dias Selecionados:", None))
+        self.pushButton_voltar_menu.setText("")
         self.label_hora.setText(QCoreApplication.translate("MainWindow", u"hhhhhh", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"Dias Selecionados:", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"Hora:", None))
         self.label_dias.setText(QCoreApplication.translate("MainWindow", u"ddddd", None))
         self.label.setText(QCoreApplication.translate("MainWindow", u"SICOGES", None))
-        self.pushButton_voltar_menu.setText("")
+        self.pushButton_relatorio.setText(QCoreApplication.translate("MainWindow", u"Salas reservadas", None))
         self.label_4.setText(QCoreApplication.translate("MainWindow", u"LISTA DE AMBIENTES", None))
         self.pushButton_Labs.setText(QCoreApplication.translate("MainWindow", u"LABORATORIOS", None))
         self.pushButton_Auditorios.setText(QCoreApplication.translate("MainWindow", u"AUDITORIOS", None))
